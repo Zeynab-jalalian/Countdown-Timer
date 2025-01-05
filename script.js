69b803ececee7a2488a6fc374b8525622991df01
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-05 10:09:40 +0330

    code js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 let num=document.querySelector('.num');
+let count=10;
+let T=setInterval(function time(){
+ count--;
+ num.textContent=count;
+ if(count==0){
+    num.textContent="زمان تمام شد!"
+    clearInterval (T);
+ }
+},1000)
