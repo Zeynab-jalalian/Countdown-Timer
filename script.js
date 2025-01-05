e27cf032b7c078ac5d4d4dc14cc01255240fa366
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-05 10:12:17 +0330

    finish

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let T=setInterval(function time(){
  num.textContent=count;
  if(count==0){
     num.textContent="زمان تمام شد!"
+    document.body.style.background='black';
     clearInterval (T);
  }
 },1000)
